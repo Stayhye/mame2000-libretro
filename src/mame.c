d1dced0a0f24049ae59036a57fd4ebaa7414898a
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-23 17:21:50 -0500

    Update mame.c

--- src/mame.c
+++ src/mame.c
@@ -168,7 +168,8 @@ int mame_start_game(int game)
 			(options.color_depth != 8 && (Machine->gamedrv->flags & GAME_REQUIRES_16BIT)))
 		Machine->color_depth = 16;
 	else
-		Machine->color_depth = 8;
+		/* Force 16-bit color depth for PS2 compatibility */
+		Machine->color_depth = 16;
 
 
     if(!iOS_fixedRes)
