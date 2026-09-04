d58b537ed1dbf1fa9c157633a1c5baf915e25323
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-04 18:15:35 -0500

    e

--- src/cheat.c
+++ src/cheat.c
@@ -1,6 +1,7 @@
+int he_did_cheat = 0;
+char *cheatfile = NULL;
+
 void StopCheat(void) {}
 void InitCheat(void) {}
-int he_did_cheat = 0;
 void DoCheat(void) {}
 void cheat_menu(void) {}
-char *cheatfile = NULL;
